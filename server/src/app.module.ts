9dfa890f52cd79ec8c4641c8f59c691661f5158e
ilmahdi <eabdelha@student.1337.ma>
2023-07-10 10:41:13 +0100

    package.json

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
-    UserModule, PrismaModule, AuthModule, 
+    UserModule, PrismaModule, AuthModule, ChatModule, 
   ],
   controllers: [],
   providers: [],
