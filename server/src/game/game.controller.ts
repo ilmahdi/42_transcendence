fe6520e14f41200e73a7d4ec870222ef00f7f3ec
El mahdi Abdelhadi <eabdelha@e3r10p2.1337.ma>
2023-09-24 12:13:25 +0100

    store game

--- server/src/game/game.controller.ts
+++ server/src/game/game.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/utils/guards/jwt.guard';
+import { GameService } from './game.service';
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+
+  constructor(private readonly gameService: GameService) {}
+
+    @Get("history")
+    @UseGuards(JwtGuard)
+    getMatchHistory() {
+        return this.temporariusService.getMatchHistory();
+    }
+}
