ab3d3e2c57d69b230b65a4a12724a9de5c849290
El mahdi Abdelhadi <eabdelha@e3r9p2.1337.ma>
2023-09-29 12:25:41 +0100

    docker

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: true,
-  baseApiUrl: 'http://10.13.11.10:3000/api',
-  apiUrl: 'http://10.13.11.10:3000',
-  apiUrlAuth: 'http://10.13.11.10:3000/api/auth/login/42/',
-  uploadUrl: 'http://10.13.11.10:3000/api/user/avatar/',
+  baseApiUrl: 'http://10.13.9.2:3000/api',
+  apiUrl: 'http://10.13.9.2:3000',
+  apiUrlAuth: 'http://10.13.9.2:3000/api/auth/login/42/',
+  uploadUrl: 'http://10.13.9.2:3000/api/user/avatar/',
 };
 
 /*
---
+++ server/tools/entry.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+npx prisma db push --accept-data-loss 
+
+npm run start:dev
