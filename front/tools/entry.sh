cc21c7db18bbbb3ea7e7b80365e25b2cbd0215a7
El mahdi Abdelhadi <eabdelha@e3r9p4.1337.ma>
2023-09-30 09:41:07 +0100

    docker done

--- front/tools/entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-ng build --configuration production
-
-exec ng serve --configuration production --host 0.0.0.0
--- server/tools/entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-npx prisma db push --accept-data-loss 
-
-npm run start:dev
